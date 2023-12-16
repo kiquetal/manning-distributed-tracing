6a19a46505f7a0e279fd040ce9f5ca915b275fab
kiquetal <kiquetal@gmail.com>
2023-12-15 21:26:11 -0300

    Adding env variable pointing to a jaeger client

--- tracing-demo/src/main/java/manning/learning/tracingdemo/Controller.java
+++ tracing-demo/src/main/java/manning/learning/tracingdemo/Controller.java
@@ -1,21 +1,30 @@
 package manning.learning.tracingdemo;
 
 
+import io.opentracing.Span;
+import io.opentracing.Tracer;
+import io.opentracing.util.GlobalTracer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.module.Configuration;
 
 @RestController
 public class Controller
 {
 
+    @Autowired
+    Tracer tracer;
     Logger log = LoggerFactory.getLogger(Controller.class);
     @RequestMapping("/checkout")
     public String checkout()
     {
+        Span checkoutSpan = tracer.buildSpan("checkout").start();
         log.info("Checkout");
+        checkoutSpan.finish();
         return "Checkout";
     }
 
